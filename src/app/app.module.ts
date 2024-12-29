4764e7365805379859e67ee208d97379b4e2c0f8
marcom966 <marcom962015@gmail.com>
2024-12-29 11:12:06 +0100

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LogInComponentComponent } from './components/log-in-component/log-in-component.component';
+import { HomeComponentComponent } from './components/home-component/home-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LogInComponentComponent,
+    HomeComponentComponent
   ],
   imports: [
     BrowserModule,
